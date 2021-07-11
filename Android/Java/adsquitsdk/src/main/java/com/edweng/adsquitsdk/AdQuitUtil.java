eced5c921982c219a2a13179c115b7d07a380d43
Ed Weng <edweng@gmail.com>
2021-07-10 20:12:46 -0700

    Fixing AdQuitUtil in Android app. Ad Impression now working.

--- Android/Java/adsquitsdk/src/main/java/com/edweng/adsquitsdk/AdQuitUtil.java
+++ Android/Java/adsquitsdk/src/main/java/com/edweng/adsquitsdk/AdQuitUtil.java
@@ -23,68 +23,60 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+
 public class AdQuitUtil {
     public static final String URL = "https://ads-quit.herokuapp.com/logEvent";
     public static final MediaType JSON
             = MediaType.get("application/json; charset=utf-8");
     private static final String TAG="AdQuitUtil";
 
-    private static AdQuitUtil sharedAdQuitUtil = null;
+    public static JSONObject getEventParams(Context context) throws PackageManager.NameNotFoundException, JSONException {
+        JSONObject dictionary = new JSONObject();
+        PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
 
-    public static AdQuitUtil getInstance() {
-        if(sharedAdQuitUtil == null) {
-            sharedAdQuitUtil = new AdQuitUtil();
-        }
-        return sharedAdQuitUtil;
-    }
+        dictionary.put("package_name", pInfo.packageName);
+        dictionary.put("platform", "android");
+        dictionary.put("os_version", Build.VERSION.SDK_INT);
+        dictionary.put("app_version", pInfo.versionCode); /* Should we also capture version name? */
+        dictionary.put("device_id", IronSource.getAdvertiserId(context));
+        dictionary.put("uuid", UUID.randomUUID().toString());
+        dictionary.put("user_id", "1");
+        dictionary.put("event_name", "ad_impression_data");
 
-    public class PingTask extends AsyncTask<PingTask.DataParams, Void, String> {
-        public class DataParams {
-            public String eventName;
-            public Context context;
-            public JSONObject withData;
-        }
-
-        @Override
-        protected String doInBackground(DataParams... params) {
-            OkHttpClient client = new OkHttpClient();
-            JSONObject dictionary = new JSONObject();
-            try {
-                PackageInfo pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-
-                dictionary.put("package_name", pInfo.packageName);
-                dictionary.put("os_version", Build.VERSION.SDK_INT);
-                dictionary.put("app_version", pInfo.versionCode); /* Should we also capture version name? */
-                dictionary.put("device_id", IronSource.getAdvertiserId(context));
-                dictionary.put("uuid", UUID.randomUUID().toString());
-                dictionary.put("user_id", "1");
-                dictionary.put("event_name", eventName);
-                dictionary.put("data", withData);
-            } catch (JSONException e) {
-                Log.e(TAG, e.toString());
-            } catch (PackageManager.NameNotFoundException e) {
-                Log.e(TAG, e.toString());
-            }
-
-            RequestBody body = RequestBody.create(JSON, dictionary.toString());
-            Request request = new Request.Builder()
-                    .url(URL)
-                    .post(body)
-                    .build();
-
-            try (Response response = client.newCall(request).execute()) {
-                if (!response.isSuccessful()) Log.e(TAG, "Request was unsuccessful");
-                System.out.println(response.body().string());
-//            Log.d(TAG, response.body().string());
-            } catch(IOException e) {
-                Log.e(TAG, e.toString());
-            }
-        }
+        return dictionary;
     }
 
     public static void pingAdInfo(Context context, ImpressionData impressionData) throws IOException {
-        PingTask.DataParams params = new PingTask.DataParams();
+        AsyncTask<String, Void, String> asyncTask = new AsyncTask<String, Void, String>() {
+            JSONObject dictionary;
 
-        AdQuitUtil.PingTask.execute()
+            @Override
+            protected String doInBackground(String... params) {
+                try {
+                    dictionary = getEventParams(context);
+                    dictionary.put("data", impressionData.getAllData());
+                } catch (JSONException | PackageManager.NameNotFoundException e) {
+                    Log.e(TAG, e.toString());
+                }
+
+                OkHttpClient client = new OkHttpClient();
+                RequestBody body = RequestBody.create(JSON, dictionary.toString());
+                Request request = new Request.Builder()
+                        .url(URL)
+                        .post(body)
+                        .build();
+
+                try (Response response = client.newCall(request).execute()) {
+                    if (!response.isSuccessful()) Log.e(TAG, "Request was unsuccessful");
+                    Log.d(TAG, response.body().string());
+                } catch(IOException e) {
+                    Log.e(TAG, e.toString());
+                }
+
+                return null;
+            }
+        };
+
+        asyncTask.execute();
     }
 }
